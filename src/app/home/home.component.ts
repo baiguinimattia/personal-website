45fc86955fce253564509cffe0eaf1983f58e274
Baiguini Mattia <mattia.baiguini@gmail.com>
2020-02-01 21:19:58 +0200

    implementing resume button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  liveLink = "https://morning-meadow-06942.herokuapp.com/"
-  gitCode = "https://github.com/baiguinimattia/TVSeriesWebApp"
+  liveLink = "https://morning-meadow-06942.herokuapp.com/";
+  gitCode = "https://github.com/baiguinimattia/TVSeriesWebApp";
+  resume = "https://drive.google.com/open?id=1ALlgSlSsYB9CDdBtnmEq_TBuTtbBRzY9";
   constructor() { }
 
   ngOnInit() {
